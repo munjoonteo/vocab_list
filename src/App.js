d50a4847ebc6830a05d99c70f2960bdbf63afc6d
munjoonteo <teomunjoon101@gmail.com>
2021-01-31 16:20:14 +1100

    added api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import './App.css';
+import axios from "axios";
+import "./App.css";
+
+async function getWords() {
+  await axios
+    .get(
+      `https://cors-anywhere.herokuapp.com/https://jisho.org/api/v1/search/words?keyword=neko`
+    )
+    .then(result => {
+      console.log(result.data.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
 function App() {
+  getWords();
   return (
     <div className="App">
       <header className="App-header">
