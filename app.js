3744c461e4fa21759b6cf24b9585fd43cd7782cd
Mun Joon Teo <teomunjoon101@gmail.com>
2021-04-30 11:45:57 +1000

    Setup heroku deploy

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.post("/*", (req, res) => {
+app.post("/word", (req, res) => {
   axios
     .get(`https://jisho.org/api/v1/search/words?keyword=${req.body.word}`)
     .then(response => res.status(200).send(response.data))
     .catch(err => console.log(err));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => {
